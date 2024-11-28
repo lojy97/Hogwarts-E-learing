f3ef58ee60494a68736c963db69861fb92c5d640
lojy97 <lojine.elhwary@gmail.com>
2024-11-28 22:52:58 +0200

    db added

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,14 @@ import { QuizzesModule } from './quizzes/quizzes.module';
 import { ResponsesModule } from './responses/responses.module';
 import { ProgressModule } from './progress/progress.module';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ModuleModule } from './module/module.module';
 import { InteractionsModule } from './interactions/interactions.module';
 
 @Module({
-  imports: [StudentModule, CourseModule, QuizzesModule, ResponsesModule, ProgressModule, UserModule, ModuleModule, InteractionsModule],
+ imports: [
+  MongooseModule.forRoot('mongodb://localhost:27017/witchesDB'),
+  StudentModule, CourseModule, QuizzesModule, ResponsesModule, ProgressModule, UserModule, ModuleModule, InteractionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
