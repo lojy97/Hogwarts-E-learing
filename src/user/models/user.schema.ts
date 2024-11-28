fdab22d125335bcc1408ce9ae41c98f43de3d29c
Yousef Masanobu Ochiai <yousef.ochiai@student.giu-uni.de>
2024-11-28 23:09:43 +0200

    Users + coueses

--- src/user/models/user.schema.ts
+++ src/user/models/user.schema.ts
@@ -1,5 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
+import { Course } from '../../course/models/course.schema';
+
+export enum UserRole {
+  Student = 'student',
+  Instructor = 'instructor',
+  Admin = 'admin',
+}
 
 @Schema()
 export class User {
@@ -15,14 +22,17 @@ export class User {
   @Prop({ required: true })
   passwordHash: string;
 
-  @Prop({ required: true })
-  role: string;
+  @Prop({ required: true, enum: UserRole })
+  role: UserRole;
 
   @Prop()
   profilePictureUrl?: string;
 
   @Prop({ required: true, default: Date.now })
   createdAt: Date;
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }] })
+  courses: Course[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
