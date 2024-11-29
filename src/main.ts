65db9e34e632bcfe83dc8accd01686a82faca8b4
Hamza-4082 <hamza.hatem.salah@gmail.com>
2024-11-29 22:30:04 +0000

    Removed unnecessary console.log

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-
-  console.log(process.env.DB_URL);
-
-  
   await app.listen(3000);
 }
 bootstrap();
