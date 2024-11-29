3b6f3470c9dabf5c89863e7d504997a0678f2966
Farrag <aucyberange@gmail.com>
2024-11-29 23:52:05 +0200

    Remove debug log for database URL in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  console.log(process.env.DB_URL);
-
-  
   await app.listen(3000);
 }
 bootstrap();
