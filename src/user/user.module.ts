64574c7e92fc15870a0b0c6212a13259d637e953
Yousef Masanobu Ochiai <yousef.ochiai@student.giu-uni.de>
2024-11-30 20:18:22 +0200

    updated user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,21 +3,13 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './models/user.schema';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from 'src/auth/guards/authentication.guard';
 
 @Module({
   // Import the MongooseModule and define the User schema
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   
-  // Provide the UserService and apply the AuthGuard globally to all routes
-  providers: [
-    UserService,
-    {
-      provide: APP_GUARD, // Apply guard globally to all routes instead of specifying one by one
-      useClass: AuthGuard,
-    },
-  ],
+  // Provide the UserService
+  providers: [UserService],
   
   // Define the UserController
   controllers: [UserController]
