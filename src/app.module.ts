e85dd8495e27b92407c5f17fcfb57ddac5f800ac
Yousef Masanobu Ochiai <yousef.ochiai@student.giu-uni.de>
2024-11-30 21:05:35 +0200

    Fixed user schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CourseModule } from './course/course.module';
@@ -6,19 +8,23 @@ import { QuizzesModule } from './quizzes/quizzes.module';
 import { ResponsesModule } from './responses/responses.module';
 import { ProgressModule } from './progress/progress.module';
 import { UserModule } from './user/user.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ModuleModule } from './module/module.module';
 import { InteractionsModule } from './interactions/interactions.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-
- imports: [
-  ConfigModule.forRoot({ isGlobal: true }), // To Load .env globally
-  MongooseModule.forRoot(process.env.DB_URL),
-  CourseModule, QuizzesModule, ResponsesModule, ProgressModule, UserModule, ModuleModule, InteractionsModule, AuthModule],
-
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Load .env globally
+    MongooseModule.forRoot(process.env.DB_URL), // Use DB_URL from .env
+    CourseModule,
+    QuizzesModule,
+    ResponsesModule,
+    ProgressModule,
+    UserModule,
+    ModuleModule,
+    InteractionsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/models/user.schema.ts
+++ src/user/models/user.schema.ts
@@ -3,17 +3,15 @@ import mongoose, { HydratedDocument } from 'mongoose';
 import { Course } from '../../course/models/course.schema';
 import * as bcrypt from 'bcrypt';
 
+
 export enum UserRole {
   Student = 'student',
   Instructor = 'instructor',
   Admin = 'admin',
 }
 
-@Schema()
+@Schema({ timestamps: true }) // Enable timestamps
 export class User {
-  @Prop({ required: true })
-  userId: string;
-
   @Prop({ required: true })
   name: string;
 
@@ -23,14 +21,11 @@ export class User {
   @Prop({ required: true })
   passwordHash: string;
 
-  @Prop({ required: true, enum: UserRole, default: UserRole.Student})
+  @Prop({ required: true, enum: UserRole, default: UserRole.Student })
   role: UserRole;
 
   @Prop()
   profilePictureUrl?: string;
-
-  @Prop({ required: true, default: Date.now })
-  createdAt: Date;
 
   @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }] })
   courses: Course[];
@@ -38,11 +33,12 @@ export class User {
 
 export const UserSchema = SchemaFactory.createForClass(User);
 
+
 // Middleware to hash the password before saving the user document
 UserSchema.pre('save', async function (next) {
   if (this.isModified('passwordHash') || this.isNew) {
-      const salt = await bcrypt.genSalt(10);
-      this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
+    const salt = await bcrypt.genSalt(10);
+    this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
   }
   next();
 });
