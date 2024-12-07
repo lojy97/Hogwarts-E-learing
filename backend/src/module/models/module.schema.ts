3fb3e209bbd1e1e93c5499ca566b43a556b0dd24
lojy97 <lojine.elhwary@gmail.com>
2024-12-07 20:22:11 +0200

    quizzes dtos updated,schema updated, service code and controller (no authentication added yet )

--- backend/src/module/models/module.schema.ts
+++ backend/src/module/models/module.schema.ts
@@ -1,11 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 import {Course} from '../../course/models/course.schema';
+import {questions} from '../../questions/models/questions.schema'
 export type ModuleDocument = HydratedDocument<Module>;
 import * as mongoose from 'mongoose';
 @Schema({ timestamps: true })
 export class Module {
-  
 
   @Prop({ type: mongoose.Types.ObjectId, ref: Course.name, required: true })
   courseId: mongoose.Types.ObjectId; 
@@ -20,6 +20,18 @@ export class Module {
 
   @Prop({ default: Date.now })
   createdAt: Date;
+
+  @Prop({
+    type: String,
+    enum: ['easy', 'medium', 'hard'], 
+    required: true 
+  })
+  difficulty: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'questions', required: true })
+  questionBank_id: mongoose.Types.ObjectId;
 }
+
+
 
 export const ModuleSchema = SchemaFactory.createForClass(Module);
--- backend/src/quizzes/models/quizzes.schema.ts
+++ backend/src/quizzes/models/quizzes.schema.ts
@@ -1,20 +1,30 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
-
+import {questions} from '../../questions/models/questions.schema'
+import {Module} from '../../module/models/module.schema';
+export type quizDocument= HydratedDocument<Quiz>
 @Schema()
+
 export class Quiz{
-  @Prop({ required: true })
- 
 
-  @Prop({ required: true })
-  module_id: mongoose.Types.ObjectId;
+    @Prop({required: true , default: 0})
+    questionsITF:[number];
 
-  @Prop({ type:[{question:String,correctAnswer:String}],required: true })
-  questions: {question:string;correctAnswer:string}[];
+    @Prop({required: true, default: 0 })
+    questionsIMCQ:[number];   
 
-  @Prop({ required: true ,default:Date.now})
-  created_at: Date;
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Module', required: true })
+    Module_id: mongoose.Types.ObjectId;
 
- 
-}
+   @Prop({ required: true ,default:Date.now})
+   created_at: Date;
+  
+   @Prop({ type: [String], default: [{question:String,correctAnswer:String}] })
+    quizQuestions: {question:string;correctAnswer:string}[];
+
+  // @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'questions', required: true })
+  // quizQuestions: mongoose.Types.ObjectId;
+
+
+ }
 export const quizzesSchema = SchemaFactory.createForClass(Quiz);
