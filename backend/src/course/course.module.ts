00ad4a9e215c06486887db379442dae573ea7312
hagy2 <hagerwasef@gmail.com>
2024-12-08 20:37:30 +0200

    gu

--- backend/src/course/course.module.ts
+++ backend/src/course/course.module.ts
@@ -4,11 +4,12 @@ import { CourseService } from './course.service';
 import { CourseController } from './course.controller';
 import { Course, CourseSchema } from './models/course.schema'; 
 import { UserModule } from 'src/user/user.module';  
-
+import { AuthModule } from '../auth/auth.module';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Course.name, schema: CourseSchema }]),
     UserModule,  
+    AuthModule,
   ],
   providers: [CourseService],
   controllers: [CourseController],
--- backend/src/module/module.module.ts
+++ backend/src/module/module.module.ts
@@ -4,11 +4,12 @@ import { ModuleController } from './module.controller';
 import { ModuleService } from './module.service';
 import { Module as ModuleEntity, ModuleSchema } from './models/module.schema';
 import { UserModule } from 'src/user/user.module';  // Import UserModule
-
+import { AuthModule } from '../auth/auth.module';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: ModuleEntity.name, schema: ModuleSchema }]),
     UserModule,  
+    AuthModule,
   ],
   controllers: [ModuleController],
   providers: [ModuleService],
