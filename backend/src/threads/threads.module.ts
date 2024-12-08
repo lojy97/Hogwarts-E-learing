a305d679689b620b5022e371b123bb57e7cc41a5
Yousef Masanobu Ochiai <yousef.ochiai@student.giu-uni.de>
2024-12-08 23:28:35 +0200

    fixed threads

--- backend/src/threads/threads.module.ts
+++ backend/src/threads/threads.module.ts
@@ -5,13 +5,23 @@ import { ThreadController } from './threads.controller';
 import { Thread, ThreadSchema } from './models/threads.schema';
 import { Forum, ForumSchema } from '../forum/models/forum.schema';
 import { ReplyModule } from '../reply/reply.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Thread.name, schema: ThreadSchema },
       { name: Forum.name, schema: ForumSchema }, // Import Forum schema to update forums
     ]),
-    
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '3600s' },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [ThreadController],
   providers: [ThreadService],
