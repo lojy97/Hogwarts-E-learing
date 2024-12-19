5f2583ebcd162c68abd771ea2469574e2074a82e
lojy97 <lojine.elhwary@gmail.com>
2024-12-19 23:40:55 +0200

    my courses instructor

--- frontend/src/app/pages/instructor/courses/myCourses/page.tsx
+++ frontend/src/app/pages/instructor/courses/myCourses/page.tsx
@@ -41,7 +41,7 @@ return(
     <div className="w-full max-w-4xl">
       {myCourses.length > 0 ? (
         <ul className="space-y-4">
-          {myCourses.map((course:course, index) => (
+          {myCourses.map((course:course, index) => (course.isAvailable && (
             <li
               key={index}
               className="p-4 bg-gray-800 rounded-lg text-white shadow-md"
@@ -50,13 +50,13 @@ return(
               <p className="text-gray-400">{course.description}</p>
               <p className="text-gray-400 font-semibold">Rating:{course.averageRating}</p>
               <Link
-                href={`/pages/instructor/courses/${course._id}`}
+                href={`/pages/instructor/courses/myCourses/${course._id}`}
                 className="text-blue-400 hover:underline mt-2 inline-block"
               >
                 View Details
               </Link>
             </li>
-          ))}
+          )))}
         </ul>
       ) : (
         <p className="text-gray-400">No courses available.</p>
