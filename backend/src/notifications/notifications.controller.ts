96d4634b0aed309000e4463ad33d7ea1dbc134ba
lojy97 <lojine.elhwary@gmail.com>
2024-12-20 22:20:58 +0200

    n

--- backend/src/notifications/notifications.controller.ts
+++ backend/src/notifications/notifications.controller.ts
@@ -1,4 +1,24 @@
 import { Controller } from '@nestjs/common';
-
+import {  Get, Post, Body, Param, Delete, Put,UseGuards } from '@nestjs/common';
+import { NotificationsService } from './notifications.service';
+import { CreateNotificationDto } from './dto/createNotification.dto';
+import { NotificationssSchema,NotificationsDocument } from './models/notifications.schema';
 @Controller('notifications')
-export class NotificationsController {}
+export class NotificationsController {
+    constructor(private readonly notificationsService: NotificationsService) {}
+    @Post()
+      async create(@Body() noteData: CreateNotificationDto): Promise<NotificationsDocument> {
+        return await this.notificationsService.create(noteData);
+      }
+    
+    
+      @Get()
+      async findAll(): Promise<NotificationsDocument[]> {
+        return await this.notificationsService.findAll();
+      }
+    
+      @Get(':id')
+      async findById(@Param('id') id: string): Promise<NotificationsDocument> {
+        return await this.notificationsService.findById(id);
+      }
+}
--- backend/src/notifications/notifications.service.ts
+++ backend/src/notifications/notifications.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
-
+import { NotificationsDocument,Notifications } from './models/notifications.schema';
+import mongoose from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+     constructor(
+            @InjectModel(Notifications.name) private NotificationModel: mongoose.Model<Notifications>
+        ) {}
+     async create(notiData: Notifications): Promise<NotificationsDocument> {
+        const newNot= new this.NotificationModel(notiData);
+        return await newNot.save();
+     }
+      async findAll(): Promise<NotificationsDocument[]> {
+             let responses=await this.NotificationModel.find();
+             return responses;
+           }
+         
+           async findById(id: string): Promise<NotificationsDocument> {
+             return await this.NotificationModel.findById(id);
+           }
+}
