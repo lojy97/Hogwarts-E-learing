58201fe8294a8e5b5b5235d9f58e921f60cfa0d4
Yousef Masanobu Ochiai <yousef.ochiai@student.giu-uni.de>
2024-12-24 23:26:09 +0200

    fixed front RBAC + instructor homepage

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -3,6 +3,13 @@ import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/SignUpDto';
 import { SignInDto } from './dto/SignInDto';
 import { Response } from 'express';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from '../user/models/user.schema';
+import { Roles } from './decorators/roles.decorator';
+import { AuthGuard } from './guards/authentication.guard';
+import { RolesGuard } from './guards/authorization.guard';
+import { UserRole } from '../user/models/user.schema';
+import { UseGuards } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
@@ -60,5 +67,14 @@ export class AuthController {
       throw new BadRequestException('Token and userId are required');
     }
     return this.authService.verifyEmail(token, userId);
+  }
+
+  @Get('dummy')
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(UserRole.Instructor, UserRole.Admin)
+  dummyEndpoint() {
+    return {
+      message: 'This is a protected route for Instructors and Admins only',
+    };
   }
 }
--- frontend/src/app/pages/Admin/home/page.tsx
+++ frontend/src/app/pages/Admin/home/page.tsx
@@ -101,6 +101,7 @@ export default function AdminHomepage() {
 
   return (
     <LayoutHome>
+      <div>
       <div className="flex flex-col items-center min-h-screen bg-[#121212] p-6">
         <h1 className="text-3xl font-bold text-white mb-8">Admin Homepage</h1>
 
@@ -198,6 +199,7 @@ export default function AdminHomepage() {
             <p className="text-gray-400">No users match your search criteria.</p>
           )}
         </div>
+      </div>
       </div>
     </LayoutHome>
   );
